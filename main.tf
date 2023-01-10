606eddae9ce7144cf3a47feac282a406da49e9cb
Adam Lewandowski <adam_lewandowski@epam.com>
2023-01-10 10:23:34 +0100

    vpc_id fix

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  azs             = ["eu-central-1", "eu-central-1b", "eu-central-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog_new"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = [ "http-80-tcp", "https-443-tcp" ]
   ingress_cidr_blocks = [ "0.0.0.0/0" ]
