4e14529ecc3abfe8ab7b6b639f9a85b38db2f150
Adam Lewandowski <adam_lewandowski@epam.com>
2023-01-10 10:10:05 +0100

    Zone correction

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-central-1", "eu-central-1b", "eu-central-1c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
